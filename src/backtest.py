04fa3305dac3ce263d2c88e771e18c8ba82d2d97
Jane DiSciullo <4mk9hy48dz@privaterelay.appleid.com>
2026-08-04 22:29:11 -0400

    Update backtest.py

--- src/backtest.py
+++ src/backtest.py
@@ -17,11 +17,11 @@ def run_backtest(price, starting_balance=100_000):
 
     price = price.copy()
 
-    price["Sys_Ret"] = np.where(
-        price["Long"].shift(1) == True,
-        price["Return"],
-        1
-    )
+price["Sys_Ret"] = np.where(
+    price["Long"].shift(1) == True,
+    1 + price["Return"],
+    1
+)
 
     price["Sys_Bal"] = (
         starting_balance * price["Sys_Ret"].cumprod()
